997934f06151bea4797a5b1980882cef60be8f70
Budi Irawan <deerawan@gmail.com>
2017-12-27 23:04:46 +1100

    feat: add flow, eslint

--- slushfile.js
+++ slushfile.js
@@ -1,34 +1,19 @@
 'use strict';
 
 const gulp = require('gulp');
-const conflict = require('gulp-conflict');
 const template = require('gulp-template');
-const rename = require('gulp-rename');
+const conflict = require('gulp-conflict');
 const inquirer = require('inquirer');
 const npmInstallPackage = require('npm-install-package');
 
 gulp.task('default', function (done) {
-  const prompts = [{
-    type: 'confirm',
-    name: 'flow',
-    message: 'Do you want to use flow?',
-    default: true
-  }]
-
-  inquirer.prompt(prompts, function (answers) {
-    const eslint = answers.eslint;
-
+  inquirer.prompt(questions).then(answers => {
     const templates = [
       __dirname + '/templates/**/**'
-    ]
+    ];
 
     gulp
       .src(templates)
-      .pipe(rename(function (file) {
-        if (file.basename[0] === '@') {
-          file.basename = file.basename.slice(1);
-        }
-      }))
       .pipe(conflict('./'))
       .pipe(gulp.dest('./'))
       .on('end', function () {
@@ -38,13 +23,16 @@ gulp.task('default', function (done) {
           'eslint-plugin-import',
           'eslint-plugin-jsx-a11y',
           'eslint-plugin-react',
-          'eslint-config-airbnb'
-        ]
+          'eslint-config-airbnb',
+          'flow-bin',
+          'babel-preset-flow',
+          'babel-preset-react'
+        ];
 
         const installOptions = {
           saveDev: true,
           cache: true
-        }
+        };
 
         npmInstallPackage(devDeps, installOptions, done);
       });
